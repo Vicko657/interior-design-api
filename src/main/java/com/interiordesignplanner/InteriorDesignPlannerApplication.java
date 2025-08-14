fa39067c7182b1af11477f1d855f58deea5b1632
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-08-14 19:58:06 +0100

    Initialise Spring Boot in the project (closes #3)

---
+++ src/main/java/com/interiordesignplanner/InteriorDesignPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.interiordesignplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorDesignPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteriorDesignPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interiordesignplanner/InteriorDesignPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interiordesignplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorDesignPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
