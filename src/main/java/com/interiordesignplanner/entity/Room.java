675a53a080d8d26c84a75ddca9292f3547ff6ed7
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-08-26 18:21:50 +0100

    Updated Room entity with setters and getters (#6 #16)

--- src/main/java/com/interiordesignplanner/entity/Room.java
+++ src/main/java/com/interiordesignplanner/entity/Room.java
@@ -29,6 +29,7 @@ public class Room {
     @JoinColumn(name = "project_id", referencedColumnName = "id")
     private Project project; // Project foreign key
 
+    private String roomName;
     private Double length;
     private Double height;
     private Double width;
@@ -41,5 +42,90 @@ public class Room {
     private Instant createdAt; // Date the Room was linked to the project
     @LastModifiedDate
     private Instant updatedAt; // Updates the date, each time the room's information is modified
+
+    // Room | Constructor
+    public Room(String roomName, Double length, Double height, Double width, String unit, String checklist,
+            String changes) {
+
+        this.roomName = roomName;
+        this.length = length;
+        this.height = height;
+        this.width = width;
+        this.unit = unit;
+        this.checklist = checklist;
+        this.changes = changes;
+
+    }
+
+    // Room | Parameterless constructor
+    public Room() {
+        this("Room Name:", 7.5, 4.5, 5.5, "m", "Room checklist", "Room changes");
+    }
+
+    // Room | Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public Double getLength() {
+        return length;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public String getChecklist() {
+        return checklist;
+    }
+
+    public String getChanges() {
+        return changes;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    // Room | Setters
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public void setLength(Double length) {
+        this.length = length;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+
+    public void setChecklist(String checklist) {
+        this.checklist = checklist;
+    }
+
+    public void setChanges(String changes) {
+        this.changes = changes;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 
 }
