75aefe47b49e5612672067ff1fce3898b0062d8c
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-09-04 19:41:15 +0100

    Refactor: Added JSONIgnore in the for the Ids in the Abstract Entity (#20)

--- src/main/java/com/interiordesignplanner/AbstractEntity.java
+++ src/main/java/com/interiordesignplanner/AbstractEntity.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +20,7 @@ public abstract class AbstractEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id; // Entity's Id
 
     // JPA Auditing
