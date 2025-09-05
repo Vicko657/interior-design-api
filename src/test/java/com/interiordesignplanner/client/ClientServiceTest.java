1979d36409e0043b1bd57a01f09a016198cdfaf2
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-09-05 13:57:41 +0100

    Testing: Client Service Test | Test 1: Testing if all getAllClients method checking if I get a empty list back (#5)

--- src/test/java/com/interiordesignplanner/client/ClientServiceTest.java
+++ src/test/java/com/interiordesignplanner/client/ClientServiceTest.java
@@ -1,5 +1,38 @@
 package com.interiordesignplanner.client;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class ClientServiceTest {
+
+    public ClientService cService;
+    public ClientRepository cRepository;
+    public Client client1, client2;
+
+    @BeforeEach
+    public void setUp() {
+        cRepository = Mockito.mock(ClientRepository.class);
+        cService = new ClientService(cRepository);
+        client1 = new Client("FirstName1", "LastName1", "Email1", "PhoneNumber1",
+                "Address1",
+                "Notes1");
+        client2 = new Client("FirstName2", "LastName2", "Email2", "PhoneNumber2", "Address2",
+                "Notes2");
+
+    }
+
+    @Test
+    @DisplayName("Gets all of the clients in the database")
+    public void testGetAllClientsIntiallyEmpty() {
+        Mockito.when(cRepository.findAll()).thenReturn(List.of());
+        assertTrue(cService.getAllClients().isEmpty());
+
+    }
 
 }
