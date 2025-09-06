c47fe00e29798c4e53778b26255c16eb152aa78f
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-09-06 17:26:21 +0100

    Testing: Project Service Test | Test 1: Added GetAllProjectsTest(#5)

---
+++ src/test/java/com/interiordesignplanner/project/ProjectServiceTest.java
@@ -0,0 +1,61 @@
+package com.interiordesignplanner.project;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.interiordesignplanner.client.ClientService;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName(value = "Project Service Test Suite")
+public class ProjectServiceTest {
+
+    @Mock
+    public ProjectRepository pRepository;
+
+    @InjectMocks
+    public ProjectService pService;
+
+    @InjectMocks
+    public ClientService cService;
+
+    public Project project1, project2;
+
+    @BeforeEach
+    public void setUp() {
+        pService = new ProjectService(pRepository, cService);
+        project1 = new Project("Industrial Loft Redesign", 20000,
+                "Exposed brick walls, metal fixtures, and reclaimed wood accents",
+                "https://meet.google.com/hyd-ken-csa",
+                LocalDate.of(2025, 07, 20), LocalDate.of(2026, 01, 25));
+        project2 = new Project("Luxury Master Bedroom", 5000,
+                "Custom wardrobes, soft lighting, and premium fabrics for a hotel-like feel.",
+                "https://meet.google.com/lhv-erf-oub", LocalDate.of(2025, 11, 10), LocalDate.of(2026, 5, 5));
+
+    }
+
+    @Test
+    @DisplayName("GetAllProjects: Returns empty list")
+    public void testGetAllProjectsIntiallyEmpty() {
+        // Arrange
+        List<Project> projects = Collections.emptyList();
+        Mockito.when(pRepository.findAll()).thenReturn(projects);
+
+        // Act
+        List<Project> result = pService.getAllProjects();
+
+        // Assert
+        assertThat(result).isEqualTo(projects);
+
+    }
+
+}
