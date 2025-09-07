f8c3a9ee89e51720caa57c19472736bd9eabb515
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-09-07 23:45:58 +0100

    update documentaion

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.interiordesignplanner"},{"l":"com.interiordesignplanner.client"},{"l":"com.interiordesignplanner.project"},{"l":"com.interiordesignplanner.room"}];updateSearchResults();
