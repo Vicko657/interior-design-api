914afb42e4801b800f20b30a003cef0271e3d316
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-09-07 04:11:21 +0100

    Documentation | Added comment for enums (#1)

--- src/main/java/com/interiordesignplanner/project/ProjectStatus.java
+++ src/main/java/com/interiordesignplanner/project/ProjectStatus.java
@@ -1,5 +1,6 @@
 package com.interiordesignplanner.project;
 
+/** Enum for different types of project statuses */
 public enum ProjectStatus {
     PLANNING, ACTIVE, ON_HOLD, COMPLETED, CANCELLED, ARCHIVED;
 }
--- src/main/java/com/interiordesignplanner/room/RoomType.java
+++ src/main/java/com/interiordesignplanner/room/RoomType.java
@@ -1,6 +1,8 @@
 package com.interiordesignplanner.room;
 
+/** Enum for different types of room types */
 public enum RoomType {
+    
     HALLWAY, BATHROOM, LIVING_ROOM, KITCHEN,
     DINING_ROOM, UTILITY_ROOM, BEDROOM,
     CONSERVETORY, BASEMENT, LOFT;
